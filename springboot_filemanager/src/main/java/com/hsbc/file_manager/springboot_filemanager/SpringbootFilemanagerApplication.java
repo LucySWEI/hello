fcdaa383f39bf87f9395404e96ed145f2f39281b
LucySWEI <634156878@QQ.COM>
2018-08-07 12:53:02 +0800

    first commit

---
+++ springboot_filemanager/src/main/java/com/hsbc/file_manager/springboot_filemanager/SpringbootFilemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.hsbc.file_manager.springboot_filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFilemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFilemanagerApplication.class, args);
+	}
+}
